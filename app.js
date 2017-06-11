404cb26b259e59476ecd616559e9b4176b0e0d9f
Hari <sunhari@deloitte.com>
2017-06-11 20:05:04 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,61 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    xlsx = require('xlsx'),
+    pdfDocument = require('pdfkit'),
+    path = require('path');
+app.use(bodyParser.json());
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './excelstore/')
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
+    }
+});
+var upload = multer({ //multer settings
+    storage: storage,
+    fileFilter: function (req, file, callback) { //file filter
+        /*
+        if (['xls', 'xlsx'].indexOf(file.originalname.split('.')[file.originalname.split('.').length - 1]) === -1) {
+            return callback(new Error('Wrong extension type'));
+        }
+        */
+        callback(null, true);
+    }
+}).single('file');
+/** API path that will upload the files */
+app.post('/upload', function (req, res) {
+    upload(req, res, function (err) {
+        var filePath = req.file.path;
+        console.log(path.resolve(filePath));
+        if (err) {
+            res.json({ statusCode: 0, description: err });
+            return;
+        }
+        var workbook = xlsx.readFile(filePath);
+        console.log(workbook.SheetNames);
+        generatePDF(res);
+    });
+});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+app.listen('3000', function () {
+    console.log('Running on 3000...');
+});
+
+function generatePDF(responseObject, config, input) {
+    var doc = new pdfDocument();
+    doc.pipe(responseObject);
+    doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
+    doc.scale(0.6)
+        .translate(470, 130)
+        .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+        .fill('red', 'even-odd')
+        .restore();
+    doc.image('./batman.jpg', 100, 80, {width: 200, height: 100});
+    doc.end();
+}
