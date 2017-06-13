df4b08c63c0718bfe4e4709d815324235e82cd33
Sunil Hari <isunilhari@gmail.com>
2017-06-13 23:41:23 +0530

    Recitified File problem

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express'),
     server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
     server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     LABEL_EN = require('./locale/en'),
-    model = require('./config/model');
+    model = require('./model/model');
 app.use(bodyParser.json());
 
 var storage = multer.diskStorage({ //multers disk storage settings
@@ -41,8 +41,7 @@ app.post('/upload', function (req, res) {
             return;
         }
         var workbook = xlsx.readFile(filePath);
-        var cleanObj = readRawJson(workbook);
-        console.log(cleanObj);
+        console.log(workbook.SheetNames);
         generatePDF(res);
     });
 });
@@ -53,7 +52,6 @@ app.get('/', function (req, res) {
 */
 app.get('/', function (req, res) {
     renderPDF(res);
-    //res.sendFile(__dirname + "/index.html");
 });
 app.listen(server_port, server_ip_address, function () {
     console.log('Running on ' + server_port);
@@ -72,12 +70,55 @@ function renderPDF(response) {
         .moveTo(50, 130).lineTo(570, 130).stroke()//underline of the heading
         .font('gillsansbold').fontSize(10).text(LABEL_EN.partnerShipName, 60, 140)
         .moveTo(50, 160).lineTo(570, 160).stroke()//underline of the heading
-
-    doc.font('gillsansbold').fontSize(10).text(LABEL_EN.gpStrategy, 55, 165)//table heading column 1
-        .font('gillsansbold').fontSize(10).text(LABEL_EN.investmentRationale, 230, 165)//table heading column2
+        .font('gillsansbold').fontSize(10).text(LABEL_EN.gpStrategy, 55, 165)//table heading column 1
+        .font('gillsansbold').fontSize(10).text(LABEL_EN.investmentRationale, 210, 165)//table heading column2
         .moveTo(50, 180).lineTo(570, 180).dash(1, { space: 1 }).stroke()//dashed line  of the heading
         .font('gillsanslight').fontSize(9).text(model.partnerShipName, 55, 190)//table heading
-        .font('gillsanslight').fontSize(9).text(model.investRation, 230, 190, { align: 'justify',wordSpacing :0,lineGap:1 });
+        .font('gillsanslight').fontSize(9).text(model.investRation, 210, 190, { align: 'justify', wordSpacing: 0, lineGap: 1, width: 295 })
+        .moveTo(50, 270).undash().lineTo(570, 270).stroke()//underline of the heading
+        .font('gillsansbold').fontSize(10).text(LABEL_EN.summaryOfPartTerms, 50, 273)
+        .moveTo(50, 290).lineTo(570, 290).stroke()
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.fundSize, 50, 295)//labels
+        .font('gillsanslight').fontSize(9).text(model.fundSize, 210, 295)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.invPeriod, 340, 295)//labels
+        .font('gillsanslight').fontSize(9).text(model.investmentPeriod, 480, 295)//value
+        .moveTo(50, 310).lineTo(570, 310).dash(1, { space: 1 }).stroke()//dashed line  of the heading
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.indFocus, 50, 315)//labels
+        .font('gillsanslight').fontSize(9).text(model.industryFocus, 210, 315)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.hurdRate, 340, 315)//labels
+        .font('gillsanslight').fontSize(9).text(model.hurdleRate, 480, 315)//value
+        .moveTo(50, 330).lineTo(570, 330).dash(1, { space: 1 }).stroke()//dashed line  of the heading
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.geoFocus, 50, 335)//labels
+        .font('gillsanslight').fontSize(9).text(model.geographicFocus, 210, 335)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.manageFees, 340, 335)//labels
+        .font('gillsanslight').fontSize(9).text(model.managementFees, 480, 335)//value
+        .moveTo(50, 350).lineTo(570, 350).dash(1, { space: 1 }).stroke()//dashed line  of the heading
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.avgTransSize, 50, 355)//labels
+        .font('gillsanslight').fontSize(9).text(model.averageTransSize, 210, 355)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.carriedIns, 340, 355)//labels
+        .font('gillsanslight').fontSize(9).text(model.carriedInt, 480, 355)//value
+        .moveTo(50, 370).lineTo(570, 370).dash(1, { space: 1 }).stroke()//dashed line  of the heading
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.partLife, 50, 375)//labels
+        .font('gillsanslight').fontSize(9).text(model.partnerShipLife, 210, 375)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.proTeamSize, 340, 375)//labels
+        .font('gillsanslight').fontSize(9).text(model.professionalTeamSize, 480, 375)//value
+        .moveTo(50, 390).lineTo(570, 390).dash(1, { space: 1 }).stroke()//dashed line  of the heading
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.offices, 340, 395)//labels
+        .font('gillsanslight').fontSize(9).text(model.offices, 480, 395)//value
+        /*Next Table*/
+        .moveTo(50, 430).undash().lineTo(570, 430).stroke()//underline of the heading
+        .font('gillsansbold').fontSize(10).text(LABEL_EN.partnerPerformance, 50, 433)
+        .moveTo(50, 450).lineTo(570, 450).stroke()
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.lastReportNav, 50, 455)//labels
+        .font('gillsanslight').fontSize(9).text(model.lastReportedNav, 210, 455)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.nav, 340, 455)//labels
+        .font('gillsanslight').fontSize(9).text(model.nav, 480, 455)//value
+        .moveTo(50, 470).lineTo(570, 470).dash(1, { space: 1 }).stroke()//dashed line  of the heading
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.commitment, 50, 475)//labels
+        .font('gillsanslight').fontSize(9).text(model.commitment, 210, 475)//value
+        .font('gillsanslight').fontSize(9).text(LABEL_EN.totalIncVal, 340, 475)//labels
+        .font('gillsanslight').fontSize(9).text(model.totalValuesSinceIncep, 480, 475)//value
+        .moveTo(50, 490).lineTo(570, 490).dash(1, { space: 1 }).stroke()//dashed line  of the heading
     /*Loop Ends Here*/
     doc.end();
 
@@ -94,88 +135,88 @@ function generatePDF(responseObject, config, input) {
     doc.image('./batman.jpg', 100, 80, { width: 200, height: 100 });
     doc.end();
 }
-
-//function to read json extracted from customer's excel
+/**********Varun Change****** */
 function readRawJson(obj) {
     try {
-    var obj = obj.Sheets, cleanJson = [], lastFlag = false;
-    //overview sheet keys listed in array
-    var sheetOneKeys = ["pageNum","partnerShipName","gpStrat","investRation","fundSize","industryFocus","geographicFocus","averageTransSize","partnerShipLife","investmentPeriod","hurdleRate","managementFees","carriedInt","professionalTeamSize","offices","lastReportedNav","commitment","drawDown","percentageCalled","distributions","dpi","nav","totalValuesSinceIncep","tvpi","irrSinceIncep","cwia","currency","graphMax","graphMin"];
-    //alphaCount for iterating through column alphabets
-    //chartStart to save row number from where next chart data has to be fetched
-    //portStart to save row number from where next top portfolio data has to be fetched
-    var alphaCount = 3, chartStart = 2, portStart = 2;
-    for(var loop1 = alphaCount, alphaChar = 'C'; obj["Overview (2)"][alphaChar + '3'].w !== "#REF!";loop1++, alphaChar = convertToNumberingScheme(++alphaCount)) {
-        var newObj = {}, tempObj = obj["Overview (2)"];
-        // if there are no more investor data, set flag so that no more chart or portfolio data would be looked for
-        if(tempObj[convertToNumberingScheme(alphaCount + 1) + '3'].w === "#REF!") {
-            lastFlag = true;
+        var obj = obj.Sheets, cleanJson = [], lastFlag = false;
+        //overview sheet keys listed in array
+        var sheetOneKeys = ["pageNum", "partnerShipName", "gpStrat", "investRation", "fundSize", "industryFocus", "geographicFocus", "averageTransSize", "partnerShipLife", "investmentPeriod", "hurdleRate", "managementFees", "carriedInt", "professionalTeamSize", "offices", "lastReportedNav", "commitment", "drawDown", "percentageCalled", "distributions", "dpi", "nav", "totalValuesSinceIncep", "tvpi", "irrSinceIncep", "cwia", "currency", "graphMax", "graphMin"];
+        //alphaCount for iterating through column alphabets
+        //chartStart to save row number from where next chart data has to be fetched
+        //portStart to save row number from where next top portfolio data has to be fetched
+        var alphaCount = 3, chartStart = 2, portStart = 2;
+        for (var loop1 = alphaCount, alphaChar = 'C'; obj["Overview (2)"][alphaChar + '3'].w !== "#REF!"; loop1++ , alphaChar = convertToNumberingScheme(++alphaCount)) {
+            var newObj = {}, tempObj = obj["Overview (2)"];
+            // if there are no more investor data, set flag so that no more chart or portfolio data would be looked for
+            if (tempObj[convertToNumberingScheme(alphaCount + 1) + '3'].w === "#REF!") {
+                lastFlag = true;
+            }
+            var length = sheetOneKeys.length;
+            //loop to set values from first sheet
+            for (var keyLoop = 0; keyLoop < length; keyLoop++) {
+                newObj[sheetOneKeys[keyLoop]] = tempObj[alphaChar + '' + (keyLoop + 2)].v;
+            }
+            newObj.graphs = {
+                graph1: {
+                    xcoord: [],
+                    ycoord: ['', '0', ''],
+                    netcf: [],
+                    totalValue: [],
+                    capitalCall: [],
+                    distribution: []
+                },
+                graph2: []
+            };
+            tempObj = obj["GDATA"];
+            //loop to set values from second sheet
+            for (var loop2 = chartStart; tempObj['C' + loop2] !== undefined; loop2++) {
+                newObj.graphs.graph1.xcoord.push(tempObj['C' + loop2].v);
+                newObj.graphs.graph1.netcf.push(tempObj['D' + loop2].v);
+                newObj.graphs.graph1.totalValue.push(tempObj['E' + loop2].v);
+                newObj.graphs.graph1.capitalCall.push(tempObj['F' + loop2].v);
+                newObj.graphs.graph1.distribution.push(tempObj['G' + loop2].v);
+            }
+            for (var loop3 = -1; loop3 <= 5; loop3++) {
+                newObj.graphs.graph2.push({ label: tempObj['H' + (chartStart + loop3)].v, values: [tempObj['I' + (chartStart + loop3)].v, tempObj['J' + (chartStart + loop3)].v, tempObj['K' + (chartStart + loop3)].v] });
+            }
+            //get starting row of next chart data
+            if (!lastFlag) {
+                for (; tempObj['C' + loop2] === undefined; loop2++);
+            }
+            chartStart = loop2;
+            newObj.portfolios = [];
+            tempObj = obj["TOP5PORT"];
+            //loop to set values from third sheet
+            for (var loop4 = portStart; tempObj['C' + loop4] !== undefined; loop4++) {
+                newObj.portfolios.push([tempObj['C' + loop4].v, tempObj['D' + loop4].v, tempObj['E' + loop4].v, tempObj['F' + loop4].v, tempObj['G' + loop4].v]);
+            }
+            if (loop4 === portStart) {
+                ++loop4;
+            }
+            //get starting row for next top portfolio data
+            if (!lastFlag) {
+                for (; (loop4 - 2) % 6 !== 0; loop4++);
+            }
+            portStart = loop4;
+            //push investor data to array
+            cleanJson.push(newObj);
         }
-        var length = sheetOneKeys.length;
-        //loop to set values from first sheet
-        for(var keyLoop = 0;keyLoop < length;keyLoop++) {
-            newObj[sheetOneKeys[keyLoop]] = tempObj[alphaChar + '' + (keyLoop + 2)].v;
-        }
-        newObj.graphs = {
-            graph1: {
-                xcoord: [],
-                ycoord: ['','0',''],
-                netcf: [],
-                totalValue: [],
-                capitalCall: [],
-                distribution: []
-            }, 
-            graph2: []
-        };
-        tempObj = obj["GDATA"];
-        //loop to set values from second sheet
-        for(var loop2 = chartStart;tempObj['C' + loop2] !== undefined;loop2++) {
-            newObj.graphs.graph1.xcoord.push(tempObj['C' + loop2].v);
-            newObj.graphs.graph1.netcf.push(tempObj['D' + loop2].v);
-            newObj.graphs.graph1.totalValue.push(tempObj['E' + loop2].v);
-            newObj.graphs.graph1.capitalCall.push(tempObj['F' + loop2].v);
-            newObj.graphs.graph1.distribution.push(tempObj['G' + loop2].v);
-        }
-        for(var loop3 = -1;loop3 <= 5;loop3++) {
-            newObj.graphs.graph2.push({label:tempObj['H' + (chartStart + loop3)].v, values:[tempObj['I' + (chartStart + loop3)].v,tempObj['J' + (chartStart + loop3)].v,tempObj['K' + (chartStart + loop3)].v]});
-        }
-        //get starting row of next chart data
-        if(!lastFlag) {
-            for(;tempObj['C' + loop2] === undefined;loop2++);
-        }
-        chartStart = loop2;
-        newObj.portfolios = [];
-        tempObj = obj["TOP5PORT"];
-        //loop to set values from third sheet
-        for(var loop4 = portStart;tempObj['C' + loop4] !== undefined;loop4++) {
-            newObj.portfolios.push([tempObj['C' + loop4].v, tempObj['D' + loop4].v, tempObj['E' + loop4].v, tempObj['F' + loop4].v, tempObj['G' + loop4].v]);
-        }
-        if(loop4 === portStart) {
-            ++loop4;
-        }
-        //get starting row for next top portfolio data
-        if(!lastFlag) {
-            for(;(loop4 - 2) % 6 !== 0;loop4++);
-        }
-        portStart = loop4;
-        //push investor data to array
-        cleanJson.push(newObj);
-    }
-    }catch(e) {
+    } catch (e) {
         console.log(e);
     }
     return cleanJson;
 }
 
-var convertToNumberingScheme = function(number) {
-  var baseChar = ("A").charCodeAt(0),
-      letters  = "";
+var convertToNumberingScheme = function (number) {
+    var baseChar = ("A").charCodeAt(0),
+        letters = "";
 
-  do {
-    number -= 1;
-    letters = String.fromCharCode(baseChar + (number % 26)) + letters;
-    number = (number / 26) >> 0;
-  } while(number > 0);
+    do {
+        number -= 1;
+        letters = String.fromCharCode(baseChar + (number % 26)) + letters;
+        number = (number / 26) >> 0;
+    } while (number > 0);
 
-  return letters;
+    return letters;
 };
+/********************** */
